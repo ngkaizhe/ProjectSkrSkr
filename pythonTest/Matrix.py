db5d6638f382fa6ee9105383b40150c93e609c2e
Ng Kai Zhe <kaizhe1991@hotmail.com>
2019-03-23 01:33:24 +0800

    matrix multiplication done(without exception)

--- pythonTest/Matrix.py
+++ pythonTest/Matrix.py
@@ -3,7 +3,7 @@ class Matrix(object):
 
     # the input matrix should be one dimension list,
     #if the input matrix is in 2 dimension, then the isMatrix should be True
-    def __init__(self, matrix, rows = 0, cols = 0, isMatrix = False):
+    def __init__(self, matrix, rows = None, cols = None, isMatrix = False):
         self.matrix = []
 
         if isMatrix:
@@ -23,6 +23,9 @@ class Matrix(object):
 
     #matrix addition
     def __add__(self, other):
+        #TODO: add exception(exception class still not yet created)
+
+
         answer = []
         #C++ version1
         #for i in range(len(self.matrix)):
@@ -42,17 +45,56 @@ class Matrix(object):
         answer = [[value_self + value_other for value_self, value_other in zip(rows_self, rows_other)]for rows_self, rows_other in zip(self.matrix, other.matrix)]           
         return Matrix(answer, isMatrix = True)
     
+    #matrix subtration
+    def __sub__(self, other):
+        #TODO: add exception(exception class still not yet created)
 
+
+        answer = []
+        #C++ version1
+        #for i in range(len(self.matrix)):
+        #    temp = []
+        #    for j in range(len(self.matrix[i])):
+        #        temp.append(self.matrix[i][j] - other.matrix[i][j])
+        #    answer.append(temp)
+
+        #c++ version2 using zip
+        #for rows_self, rows_other in zip(self.matrix, other.matrix):
+        #    temp = []
+        #    for value_self, value_other in zip(rows_self, rows_other):
+        #        temp.append(value_self - value_other)
+        #    answer.append(temp)
+
+        ##python version
+        answer = [[value_self - value_other for value_self, value_other in zip(rows_self, rows_other)]for rows_self, rows_other in zip(self.matrix, other.matrix)]           
+        return Matrix(answer, isMatrix = True)
+
+    #matrix multiplication
+    def __mul__(self, other):
+        #TODO: add exception(exception class still not yet created)
+
+        answer = []
+        #c++ version
+        for i in range(len(self.matrix)):
+            temp = []
+            for j in range(len(other.matrix[0])):
+                total = 0         
+                for k in range(len(self.matrix[i])):
+                    total += self.matrix[i][k] * other.matrix[k][j]
+                temp.append(total)
+            answer.append(temp)
+
+        return Matrix(answer, isMatrix = True)
 
     def print_value(self):
-        for i in range(len(self.matrix)):
-            print(self.matrix[i])
+        for i in self.matrix:
+            print(i)
         
 
 
 if __name__== "__main__":
     mat = Matrix([1, 10, 5, 7, 8, 7, 10, 11], 2, 4)
-    mat2 = Matrix([1, 5, 6, 7, 6, 4, 2, -1], 2, 4)
+    mat2 = Matrix([1, 5, 4, 6, 7, 3, 6, 4, 2, 2, -1, 1], 4, 3)
 
-    (mat+mat2).print_value()
+    (mat*mat2).print_value()
 
